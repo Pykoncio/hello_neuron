0fe9d4824992fb4b198b9153b3c6e6fc67563637
pykoncio <hugoperalta2003@gmail.com>
2025-01-08 20:03:47 +0100

    Updated requirements.txt and fixed problems in the streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
 
 st.image("img/neurona.jpg", use_container_width=True)
 st.title("Hello Neuron!")
@@ -12,7 +10,7 @@ def calculate_output(weights, input_values, bias=0):
 
 with tab1:
     st.header("One Neuron with an Input and a Weight")
-    w0 = st.slider("Weight", min_value=0.00, max_value=5.00, value=0.5, step=0.01)
+    w0 = st.slider("s0", min_value=0.00, max_value=5.00, value=0.5, step=0.01)
     x0 = st.number_input("Insert an Input Value", value=0.00, step=0.01)
     if st.button("Calculate the output", key="tab1"):
         output = calculate_output(w0, x0)
@@ -24,13 +22,13 @@ with tab2:
         st.markdown("Weight w<sub>0</sub>", unsafe_allow_html=True)
         w0 = st.slider("s1", min_value=0.00, max_value=5.00, value=0.5, step=0.01)
         st.markdown("Input x<sub>0</sub>", unsafe_allow_html=True)
-        x0 = st.number_input("s2", value=0.00, step=0.01)
+        x0 = st.number_input("n0", value=0.00, step=0.01)
 
     with col2:
         st.markdown("Weight w<sub>1</sub>", unsafe_allow_html=True)
-        w1 = st.slider("s1", min_value=0.00, max_value=5.00, value=0.5, step=0.01)
+        w1 = st.slider("s2", min_value=0.00, max_value=5.00, value=0.5, step=0.01)
         st.markdown("Input x<sub>1</sub>", unsafe_allow_html=True)
-        x1 = st.number_input("s2", value=0.00, step=0.01)
+        x1 = st.number_input("n1", value=0.00, step=0.01)
 
     if st.button("Calculate the output", key="tab2"):
         output = calculate_output([w0, w1], [x0, x1])
@@ -41,21 +39,21 @@ with tab3:
     col1, col2, col3 = st.columns(3)
     with col1:
         st.markdown("Weight w<sub>0</sub>", unsafe_allow_html=True)
-        w0 = st.slider("s1", min_value=0.00, max_value=5.00, value=0.5, step=0.01)
+        w0 = st.slider("s3", min_value=0.00, max_value=5.00, value=0.5, step=0.01)
         st.markdown("Input x<sub>0</sub>", unsafe_allow_html=True)
-        x0 = st.number_input("s2", value=0.00, step=0.01)
+        x0 = st.number_input("n2", value=0.00, step=0.01)
 
     with col2:
         st.markdown("Weight w<sub>1</sub>", unsafe_allow_html=True)
-        w1 = st.slider("s1", min_value=0.00, max_value=5.00, value=0.5, step=0.01)
+        w1 = st.slider("s4", min_value=0.00, max_value=5.00, value=0.5, step=0.01)
         st.markdown("Input x<sub>1</sub>", unsafe_allow_html=True)
-        x1 = st.number_input("s2", value=0.00, step=0.01)
+        x1 = st.number_input("n3", value=0.00, step=0.01)
 
     with col3:
         st.markdown("Weight w<sub>2</sub>", unsafe_allow_html=True)
-        w2 = st.slider("s1", min_value=0.00, max_value=5.00, value=0.5, step=0.01)
+        w2 = st.slider("s5", min_value=0.00, max_value=5.00, value=0.5, step=0.01)
         st.markdown("Input x<sub>2</sub>", unsafe_allow_html=True)
-        x2 = st.number_input("s2", value=0.00, step=0.01)
+        x2 = st.number_input("n4", value=0.00, step=0.01)
 
     b = st.number_input("Insert Bias", value=0.00, step=0.01)
 
